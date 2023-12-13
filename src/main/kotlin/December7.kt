d7772d4d83809c483ac6b52fa7eb744311588aaa
Andrey Chausov <dxahtepb@gmail.com>
2023-12-13 04:25:55 +0300

    [December 7th] Second puzzle stub

--- src/main/kotlin/December7.kt
+++ src/main/kotlin/December7.kt
@@ -5,11 +5,14 @@ fun main() {
 class December7 : Solution() {
 
     companion object {
-        private const val CARDS = "AKQJT98765432"
+        private const val CARDS = "AKQJT98765432J"
     }
 
     private data class Hand(val hand: String, val bid: Int)
-    private data class Tester(val name: String, val test: (Map<Char, Int>) -> Boolean)
+    private data class Tester(val name: String, private val test: (Map<Char, Int>) -> Boolean) {
+        fun test(value: Map<Char, Int>) = test.invoke(value)
+    }
+    private data class TesterWithPriority(val tester: Tester, val priority: Int)
 
     private val testers = listOf(
         Tester("Five of a kind") { countCards -> countCards.size == 1 },
@@ -19,11 +22,21 @@ class December7 : Solution() {
         Tester("Two pair") { countCards -> countCards.values.sorted() == listOf(1, 2, 2) },
         Tester("One pair") { countCards -> countCards.values.sorted() == listOf(1, 1, 1, 2) },
         Tester("High card") { _ -> true },
-    ).mapIndexed { index, tester -> index to tester }
+    ).mapIndexed { index, tester -> TesterWithPriority(tester, index) }
 
-    private fun getPriority(hand: String): Int {
+    private val testersWithJokers = listOf(
+        Tester("Five of a kind") { countCards -> countCards.size == 1 },
+        Tester("Four of a kind") { countCards -> countCards.values.sorted() == listOf(1, 4) },
+        Tester("Full house") { countCards -> countCards.values.sorted() == listOf(2, 3) },
+        Tester("Three of a kind") { countCards -> countCards.values.sorted() == listOf(1, 1, 3) },
+        Tester("Two pair") { countCards -> countCards.values.sorted() == listOf(1, 2, 2) },
+        Tester("One pair") { countCards -> countCards.values.sorted() == listOf(1, 1, 1, 2) },
+        Tester("High card") { _ -> true },
+    ).mapIndexed { index, tester -> TesterWithPriority(tester, index) }
+
+    private fun getPriority(testerList: List<TesterWithPriority>, hand: String): Int {
         val countCards = hand.groupingBy { it }.eachCount()
-        return testers.first { (_, tester) -> tester.test.invoke(countCards) }.first
+        return testerList.first { it.tester.test(countCards) }.priority
     }
 
     private fun tiebreaker(left: Hand, right: Hand): Int {
@@ -35,10 +48,12 @@ class December7 : Solution() {
             .compare(left.hand.map { CARDS.indexOf(it) }, right.hand.map { CARDS.indexOf(it) })
     }
 
-    private fun compareHands(left: Hand, right: Hand): Int {
-        val priorityResult = getPriority(left.hand).compareTo(getPriority(right.hand))
-        return if (priorityResult != 0) priorityResult
-        else tiebreaker(left, right)
+    private fun getCompareHandsFunctionWithTesters(testerList: List<TesterWithPriority>): (Hand, Hand) -> Int {
+        return { left, right ->
+            val priorityResult = getPriority(testerList, left.hand).compareTo(getPriority(testerList, right.hand))
+            if (priorityResult != 0) priorityResult
+            else tiebreaker(left, right)
+        }
     }
 
     private fun parseHand(rawHandAndBid: String): Hand {
@@ -46,15 +61,19 @@ class December7 : Solution() {
         return Hand(hand, bid.toInt())
     }
 
-    override fun first() {
-        readLines("seventh.txt")
-            .map { parseHand(it) }
-            .sortedWith(::compareHands)
+    private fun countTotalWinningsWithRules(lines: List<String>, testerList: List<TesterWithPriority>) {
+        lines.map { parseHand(it) }
+            .sortedWith(getCompareHandsFunctionWithTesters(testerList))
             .reversed()
             .foldIndexed(0L) { index, acc, hand -> acc + ((index + 1) * hand.bid) }
             .also { println(it) }
     }
 
+    override fun first() {
+        println(countTotalWinningsWithRules(readLines("seventh.txt"), testers))
+    }
+
     override fun second() {
+        println(countTotalWinningsWithRules(readLines("seventh.txt"), testersWithJokers))
     }
 }
